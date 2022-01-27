3718d4fb7aabb0b013bc9cfd9c8a27e917bfa680
Sunil <Sunil.prajapati@gmail.com>
2022-01-27 11:31:28 -0500

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Sunil Prajapati on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
