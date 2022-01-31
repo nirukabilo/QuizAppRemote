79a2e1063ac1c3dff4274b14d5c10c0e785e642f
Sunil <Sunil.prajapati@gmail.com>
2022-01-31 11:04:10 -0500

    Disabled buttons when question bank is complete

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -14,20 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblIncorrectCounter: UILabel!
     @IBOutlet weak var lblSkipCounter: UILabel!
 
+    @IBOutlet weak var btnTrue: UIButton!
+    @IBOutlet weak var btnFalse: UIButton!
+    @IBOutlet weak var btnSkip: UIButton!
+    
     var qBank: [(question:String, answer:Bool)] = []
     var qCount: Int = 0
     var correctCount: Int = 0
     var incorrectCount: Int = 0
     var skipCount: Int = 0
 
+    
+    
     func loadQuestions() {
         qBank.append(("1 + 1 = 2?", true))
         qBank.append(("2 + 2 = 4?", true))
         qBank.append(("3 + 3 = 9?", false))
-        qBank.append(("4 + 4 = 12?", true))
+        qBank.append(("4 + 4 = 12?", false))
         qBank.append(("5 + 5 = 25?", false))
     }
 
+    
 
     @IBAction func btnAction(_ sender: UIButton) {
                 
@@ -35,26 +42,28 @@ class ViewController: UIViewController {
         case 1:
             if (qBank[qCount].1 == true) {
                 correctCount += 1
+                lblCorrectCounter.text = "CORRECT: \(correctCount)"
             }
             else {
                 incorrectCount += 1
+                lblIncorrectCounter.text = "INCORRECT: \(incorrectCount)"
             }
         case 2:
             if (qBank[qCount].1 == false) {
                 correctCount += 1
+                lblCorrectCounter.text = "CORRECT: \(correctCount)"
             }
             else {
                 incorrectCount += 1
+                lblIncorrectCounter.text = "INCORRECT: \(incorrectCount)"
             }
         case 3:
             skipCount += 1
+            lblSkipCounter.text = "SKIP: \(skipCount)"
         default:
             break
         }
 
-        lblCorrectCounter.text = "CORRECT: \(correctCount)"
-        lblIncorrectCounter.text = "INCORRECT: \(incorrectCount)"
-        lblSkipCounter.text = "SKIP: \(skipCount)"
         
         qCount += 1
         if (qBank.count > qCount) {
@@ -62,6 +71,9 @@ class ViewController: UIViewController {
         }
         else {
             lblQuizQuestion.text = "Good Bye!!!"
+            btnTrue.isEnabled = false
+            btnFalse.isEnabled = false
+            btnSkip.isEnabled = false
         }
     }
     
